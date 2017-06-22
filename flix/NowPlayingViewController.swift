81e0e5a8c48e27d8ebb44d7264f94494ce65fae5
Tyler Holloway <htoyll@fb.com>
2017-06-21 17:05:58 -0700

    data up

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
-    @IBOutlet weak var TableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies: [[String: Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=5f89533e24a2ff0828389c5e1cb6f8e8")!
         
@@ -29,15 +34,38 @@ class NowPlayingViewController: UIViewController {
                 print(dataDictionary)
                 
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                //loop through array to get the title out
-                for movie in movies {
-                    let title = movie["title"] as! String
-                    print(title)
+                self.movies = movies
+                self.tableView.reloadData()
                 }
             }
-        }
+        
         task.resume()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //temp
+        return movies.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)as! MovieCell
+        
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        
+        
+        return cell
+    }
+
+
+
+
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
