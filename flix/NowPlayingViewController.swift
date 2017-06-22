1161b4ad1c6a1bb1ac7c3a52c45973f78948e33a
Tyler Holloway <htoyll@fb.com>
2017-06-22 15:48:24 -0700

    added nav and back button

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -14,11 +14,12 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let refreshControl = UIRefreshControl()
+        refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         
@@ -28,6 +29,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        
         fetchMovies()
     }
     
@@ -49,7 +51,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
                 self.tableView.reloadData()
-                
+                self.refreshControl.endRefreshing()
             }
         }
         
